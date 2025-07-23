5f6cc548d3a602add3eaf7497f5913d96e77c260
shashank <shashankraj007281@gmail.com>
2025-07-23 21:40:03 +0530

    fixed loader splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,29 @@ import App from './App.tsx'
 const hideLoader = () => {
   const loader = document.getElementById("loader");
   const loaderCSS = document.getElementById("loader-css");
+  console.log("function called after load")
   if (loader && loaderCSS) {
     loader.style.opacity = "0";
     loader.style.pointerEvents = "none";
 
-    setTimeout(() => { loader.remove(); loaderCSS.remove() }, 500); // remove from DOM for cleanliness
+    setTimeout(() => {
+      loader.remove();
+      loaderCSS.remove()
+    }, 500); // remove from DOM for cleanliness
+    console.log("loader removed")
   }
 
 };
+
+
+if (document.readyState === "complete") {
+  hideLoader();
+} else {
+  window.addEventListener("load", hideLoader);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-window.addEventListener("load", hideLoader);
